c95dcb20580412d8afa3a32cf5491f5ca115dff7
kikuchi takuya <kiiro.yellow.amarero@gmail.com>
2020-07-21 21:47:19 +0900

    文字数を表示する行を1行にした

--- text_size_count.js
+++ text_size_count.js
@@ -1,16 +1,15 @@
 //カウント
 function textLengthShow(str){
-    //改行あり
-    document.getElementById('text-size').innerHTML = "字数(改行あり)："　+ str.length + "文字";
 
-    //改行なし
+    //文字数のみ
     var count = 0;
     for(let i = 0; i < str.length; i++){
         if(str[i] == '\n'){
             count += 1;
         }
     }
-    document.getElementById('text-size-noLine').innerHTML = "字数(改行なし)："　+ (str.length - count) + "文字";
+
+    document.getElementById('text-size').innerHTML = "字数："　+ (str.length - count) + "文字";
 
 }
 
@@ -21,9 +20,7 @@ var resetBtn = document.getElementById('reset-btn');
 resetBtn.addEventListener('click', function(){
     document.getElementById('text-area').value = '';
 
-    document.getElementById('text-size').innerHTML = "字数(改行あり)："　+ 0 + "文字";
-
-    document.getElementById('text-size-noLine').innerHTML = "字数(改行なし)："　+ 0 + "文字";
+    document.getElementById('text-size').innerHTML = "字数："　+ 0 + "文字";
 }, false)
 
 //コピー処理
